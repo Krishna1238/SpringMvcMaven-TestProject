a938a8a963973efd3c1d25d7c0354b8fd7512986
sadanand <sada.sadanand03@gmail.com>
2019-07-07 15:07:53 +0530

    First commit

---
+++ src/main/java/com/nit/controller/EmployeeCntroller.java
@@ -0,0 +1,25 @@
+package com.nit.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.nit.service.EmployeeService;
+
+@Controller
+public class EmployeeCntroller {
+	
+	
+	@Autowired
+	private EmployeeService service;
+	
+	@GetMapping("/wish.htm")
+	public String wish(Model model) {
+		   
+		     String msg=service.getWish();
+		     model.addAttribute("msg",msg);
+		      return "employeedetails";
+	}
+
+}
---
+++ src/main/java/com/nit/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.nit.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+	
+	public String getWish() {
+		
+		
+		return "Good Morning..";
+	}
+
+}
